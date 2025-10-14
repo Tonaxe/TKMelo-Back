1241023d64fc1166ed6964fb36455f33fbfccfea
Tonaxe <ymennana@elpuig.xeill.net>
2025-10-14 23:33:25 +0200

    4 commit

--- TKMelo.Library/DTOs/Auth/VerifyEmailRequest.cs
+++ TKMelo.Library/DTOs/Auth/VerifyEmailRequest.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TKMelo.Library.DTOs.Auth
+﻿namespace TKMelo.Library.DTOs.Auth
 {
     public class VerifyEmailRequest
     {
